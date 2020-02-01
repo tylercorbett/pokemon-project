558b7fa4174e2e023dc2f8aaa1782e452f8cd673
Tyler Corbett <thetylercorbett@gmail.com>
2020-02-01 14:49:57 -0800

    tracking user input successfully

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [currentUserInput, setCurrentUserInput] = useState(null);
+
   const handleGuessClick = () => {
-    alert('nice guess cunt');
+    alert('You Guessed X');
   };
 
 
   return (
     <div className='App'>
      <section className='lives-left'>
-       <h2>Lives Left: 3</h2>
+       <h2>Lives Left:</h2>
      </section>
      <section className='user-guess'>
-       <h2>Enter Your Guess:</h2>
-       <input type='number' min={1} max={10}/>
+       <h2>Enter Your Guess: {currentUserInput}</h2>
+       <input type='number' min={1} max={10} onChange={event => setCurrentUserInput(event.target.value)} />
      </section>
 
      <section className='confirm-guess'>
