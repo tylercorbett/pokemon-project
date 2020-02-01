a985c530d80ef4792333f3ae645042fa969c806b
Tyler Corbett <thetylercorbett@gmail.com>
2020-02-01 12:43:46 -0800

    border box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './images/logo.svg';
-import './App.scss';
+import styles from './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Joey's First Coding Lesson
