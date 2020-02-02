939b50b460ac1efaeae1ad0122c27bded3d1f118
Tyler Corbett <thetylercorbett@gmail.com>
2020-02-01 17:11:21 -0800

    created array of guessed numbers

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
   const [currentUserInput, setCurrentUserInput] = useState(null);
   const [currentLivesLeft, setCurrentLivesLeft] = useState(3);
   const [numberToGuess] = useState(Math.floor(Math.random() * (10 - 1)) + 1);
+  const [numbersGuessed, setNumbersGuessed] = useState([]);
 
   const handleGuessClick = () => {
     if (currentLivesLeft === 0) {
-      return
+      return;
     }
-
     if (currentUserInput > 10 || currentUserInput < 1) {
-      alert('you think you are clever you cunt'); 
+      alert('Hey! Pick a number between 1 and 10!'); 
+      return;
+    }
+    if (numbersGuessed.includes(currentUserInput)) {
+      alert('You cannot put the same number twice! Try again!'); 
       return;
     } 
+
+    const newArray = [currentUserInput];
+    numbersGuessed.forEach(guess => newArray.push(guess));
+    setNumbersGuessed(newArray);
+
 
     if (currentUserInput > numberToGuess) {
        setCurrentLivesLeft(currentLivesLeft - 1);
